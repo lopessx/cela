2a39f69930d02d2064b0ec006d33d8f60fc7e5a0
Emanuel Lopes <warlopess@gmail.com>
2024-07-14 16:17:39 +0000

    feat: Innitial login and product crud

---
+++ resources/views/category/edit.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <!-- Simplicity is the essence of happiness. - Cedric Bledsoe -->
+    <h1>Category edit</h1>
+</div>
---
+++ resources/views/product/edit.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <!-- Simplicity is the essence of happiness. - Cedric Bledsoe -->
+     <h1>Product edit</h1>
+</div>
